12dcd05efec4f89713e14f10f9b2ac007e4a0420
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-11-14 10:09:34 +0100

    rename js file and move to js folder

---
+++ js/themeSwitcher.js
@@ -0,0 +1,16 @@
+const input = document.querySelector(".theme-switcher input");
+
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+    document.body.setAttribute("data-theme", savedTheme);
+    input.checked = savedTheme === "dark";
+}
+
+
+input.addEventListener("change", (e) => {
+    const theme = e.target.checked ? "dark" : "light";
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+});
